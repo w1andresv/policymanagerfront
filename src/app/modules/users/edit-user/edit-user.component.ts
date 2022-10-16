6714da8c7968387029acbe59c622f44beaedfb89
weymar <weymar.vargas@eventsite.com.co>
2022-10-16 20:48:05 +0200

    user form

--- src/app/modules/users/edit-user/edit-user.component.ts
+++ src/app/modules/users/edit-user/edit-user.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-edit-user',
-  templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.scss']
-})
+@Component( {
+    selector: 'app-edit-user',
+    templateUrl: './edit-user.component.html',
+    styleUrls: [ './edit-user.component.scss' ]
+} )
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+    formulario!: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor( private formBuilder: FormBuilder ) {
+    }
 
+    ngOnInit(): void {
+        this.loadForm();
+    }
+
+    onSubmit() {
+        if ( this.formulario.valid ) {
+            const value = this.formulario.value;
+            console.log( value );
+        }
+    }
+
+    private loadForm() {
+        this.formulario = this.formBuilder.group( {
+            username: [ null, [ Validators.required ] ],
+            password: [ null, [ Validators.required ] ],
+            habilitado: [ null, [ Validators.required ] ],
+            rol: [ null, [ Validators.required ] ],
+            celular: [ null ],
+            email: [ null, [ Validators.required ] ],
+            nombre: [ null, [ Validators.required ] ],
+            usuario: [ null ]
+        } );
+    }
 }
